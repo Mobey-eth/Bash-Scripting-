808462c25886c6373f678a67441de2cb63e77683
mobey-eth <chantler.aob@gmail.com>
2024-07-03 12:46:50 +0100

    commit 6.1

--- mobi.sh
+++ mobi.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-path="export PATH=/opt/homebrew/bin:$PATH"
+
+
+package="htop"
+
+
+if command -v $package
+then
+    echo "The package '$package' is installed."
+    echo "Uninstalling $package..."
+    brew uninstall $package
+    brew cleanup
+
+    echo "The exit code for $package unistall is $?"
+    echo "Unistall time: $(date)"
+else
+    echo "The package $package is not installed."
+    echo "Attempting to install $package."
+
+    brew install $package
+    
+    if [ $? -eq 0 ]
+    then
+        echo "The package $package has been installed."
+        sudo $package
+    else
+        echo "The package $package could not be installed."
+    fi
+
+    echo "The exit code for this script is: $?"
+    
+fi
+
+# path="export PATH=/opt/homebrew/bin:$PATH"
+
